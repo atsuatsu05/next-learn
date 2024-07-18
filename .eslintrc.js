940c681dd6382c5bc888fa8a8d70ab4c2535afca
あつみ <>
2024-07-18 21:03:23 +0900

    Lesson1

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+/** @type {import('eslint').ESLint.ConfigData} */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "next/core-web-vitals",
+    "prettier"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json"
+  },
+  plugins: [
+    "@typescript-eslint"
+  ],
+  root: true,
+  ignorePatterns: [
+    "prettier.config.js",
+    "next.config.mjs",
+    ".eslintrc.js",
+    ".next/**",
+  ],
+  rules: {
+    // 個別に設定をする
+    //error,off,wormのサン亜pターンで設定できる
+  }
+};
---
+++ src/app/lesson1/lesson1-1/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+  return (
+    <h1>Lesson1-1 ページ</h1>
+  )
+}
