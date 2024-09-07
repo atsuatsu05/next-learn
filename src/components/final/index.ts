e0451d9735708ea83a0701787ade0483f90cc3b7
あつみ <>
2024-09-07 18:35:35 +0900

    最終課題　featuresコンポーネント・styles作成

---
+++ src/components/final/index.ts
@@ -0,0 +1,15 @@
+export { Header } from "./mogumogu/header";
+export { Footer } from "./mogumogu/footer";
+export { MainImage } from "./mogumogu/main-image";
+export { Wrapper } from "./mogumogu/wrapper";
+export { Section } from "./mogumogu/section";
+export { SectionTitle } from "./mogumogu/section-title";
+export { Item } from "./mogumogu/item";
+export { LinkText } from "./mogumogu/link-text";
+export { Text } from "./mogumogu/text";
+export { PageTitle } from "./mogumogu/page-title";
+export { Container } from "./mogumogu/container";
+export { DefinitionList, DefinitionItem } from "./mogumogu/definition-list";
+export { HeaderIcon } from "./mogumogu/header-icon";
+export { MenuItem } from "./mogumogu/menu-item";
+export { PageButton } from "./mogumogu/page-button";
---
+++ src/features/final/mogumogu/contact-form/index.tsx
@@ -0,0 +1,94 @@
+"use client";
+import React, { useState } from "react";
+import styles from "./styles.module.scss";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { PageButton } from "@/components/final";
+
+import {
+    contactFormSchema,
+    type ContactFormInput,
+} from "@/libs/challenge/furniture/validation-schema";
+import { ContactInput } from "@/components/final/mogumogu/contact-input";
+import { ContactTextArea } from "@/components/final/mogumogu/contact-textarea";
+import { ContactSelect } from "@/components/final/mogumogu/contact-select";
+import { ContactFormAction } from "@/actions/challenge/furniture/contact-form";
+import { ServerErrorText } from "@/components/challenge/furniture/serverError-text";
+
+export const ContactForm: React.FC = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<ContactFormInput>({
+        mode: "onBlur",
+        resolver: zodResolver(contactFormSchema),
+    });
+    const [serverError, setServerError] = useState(false);
+    const router = useRouter();
+
+    //送信ボタンが押された時の処理
+    const onSubmit = async (data: ContactFormInput) => {
+        const result = await ContactFormAction(data);
+        // サブミット後の処理を記載
+        if (!result) {
+            setServerError(true);
+            console.log("不明なエラー");
+            return;
+        }
+
+        //resultにエラーが入っていたら（バリデーションの条件に引っ掛かったら）
+        if (result.error) {
+            setServerError(true);
+            console.log(result.error);
+            return;
+        }
+        //どのエラーにも引っ掛からなかったら正常の処理をする
+        setServerError(false);
+        console.log(data);
+        router.push("/challenge/furniture/contact/complete");
+    };
+
+    return (
+        <>
+            {serverError && (
+                <ServerErrorText>
+                    不正な値が送信されました。再度ご入力ください
+                </ServerErrorText>
+            )}
+            <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+                <ContactSelect />
+                <ContactInput
+                    label="お名前"
+                    type="text"
+                    {...register("name")}
+                />
+
+                {/* <input type="text" {...register("name")} /> */}
+                {errors.name && (
+                    <div className={styles.error}>{errors.name.message}</div>
+                )}
+                <ContactInput
+                    label="メールアドレス"
+                    type="email"
+                    {...register("email")}
+                />
+                {errors.email && (
+                    <div className={styles.error}>{errors.email.message}</div>
+                )}
+
+                <ContactTextArea
+                    label="お問い合わせ"
+                    {...register("inquiry")}
+                />
+                {errors.inquiry && (
+                    <div className={styles.error}>{errors.inquiry.message}</div>
+                )}
+            </form>
+            <div className={styles.submitArea}>
+                <PageButton>送信</PageButton>
+            </div>
+        </>
+    );
+};
